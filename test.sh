fdf2541d05bbcbfb46612071244850bcb93fe6eb
taki <nbjwl@live.com>
2020-01-22 02:33:40 +0800

    init

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+export image=$1
+export tag=$2
+export type=$3
+export http_proxy=${http_proxy}
+export https_proxy=${http_proxy}
+export no_proxy=${no_proxy}
+export mirror=true
+molecule $4
