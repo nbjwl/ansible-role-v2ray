57e7d5cda3abf5f9691b162244a3b69fb0eee357
taki <nbjwl@live.com>
2020-01-22 03:04:07 +0800

    Update Test Case

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,5 +1,5 @@
 import os
-
+import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -12,6 +12,7 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
 
 @pytest.mark.parametrize("service", [
     "v2ray"
